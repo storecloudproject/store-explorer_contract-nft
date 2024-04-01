fd145442bca478113557e88859cc72af2cf95a13
Sohaib Ahsan <sohaibahsan007@live.com>
2024-04-02 01:15:15 +0500

    network migrated.

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,11 +12,11 @@ const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY,ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.8",
-   defaultNetwork: "goerli",
+   defaultNetwork: "sepolia",
    networks: {
     hardhat: {},
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
     },
     ethereum: {
